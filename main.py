ab097c1f6c2605a42418c4081926ca30b72cd0ac
Julian <110119255+Julianragnarjimenez@users.noreply.github.com>
2023-09-13 13:45:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,76 @@
 import os
 import readchar
+import random
 
-def clear_screen():
-    os.system('cls' if os.name=='nt' else 'clear')
+class Juego:
+    def __init__(self, mapa, start, end):
+        self.mapa = mapa
+        self.start = start
+        self.end = end
+        self.px, self.py = start
 
-def create_maze_matrix(maze_str):
-    # Convertir el laberinto en una matriz de caracteres
-    maze_matrix = [list(row) for row in maze_str.strip().split("\n")]
+    def clear_terminal(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-    # Completar los dos caracteres de paredes faltantes al final
-    for row in maze_matrix:
-        row.append("#")
-    maze_matrix.append(["#" for _ in range(len(maze_matrix[0]))])
+    def display_map(self):
+        self.clear_terminal()
+        for row in self.mapa:
+            print(''.join(row))
 
-    return maze_matrix
+    def game_loop(self):
+        while (self.px, self.py) != self.end:
+            self.display_map()
+            key = readchar.readkey()
+            if key == '\x1b[A':  # Tecla de flecha arriba
+                new_px, new_py = self.px - 1, self.py
+            elif key == '\x1b[B':  # Tecla de flecha abajo
+                new_px, new_py = self.px + 1, self.py
+            elif key == '\x1b[C':  # Tecla de flecha derecha
+                new_px, new_py = self.px, self.py + 1
+            elif key == '\x1b[D':  # Tecla de flecha izquierda
+                new_px, new_py = self.px, self.py - 1
+            else:
+                continue
 
-def show_maze(maze):
-    clear_screen()
-    for row in maze:
-        print("".join(row))
+            if (
+                0 <= new_px < len(self.mapa) and
+                0 <= new_py < len(self.mapa[0]) and
+                self.mapa[new_px][new_py] != "#"
+            ):
+                self.mapa[self.px][self.py] = "."
+                self.px, self.py = new_px, new_py
+                self.mapa[self.px][self.py] = "P"
 
-def main_loop(maze, start, end):
-    px, py = start  # Coordenadas del jugador al inicio, que es la posición inicial.
+class JuegoArchivo(Juego):
+    def __init__(self, map_file):
+        mapa, start, end = self.parse_map_file(map_file)
+        super().__init__(mapa, start, end)
 
-    while (px, py) != end:
-        show_maze(maze)
-        key = readchar.readkey()
+    def parse_map_file(self, map_file):
+        with open(map_file, 'r') as f:
+            lines = f.readlines()
 
-        if key == 'n':
-            number += 1
-            if number > 50:
-                number = 50
-            clear_screen()
-            print(f"Tecla presionada: {key}")
-            print(f"Número: {number}")
-        elif key == readchar.key.UP:
-            new_px, new_py = px - 1, py
-        elif key == readchar.key.DOWN:
-            new_px, new_py = px + 1, py
-        elif key == readchar.key.LEFT:
-            new_px, new_py = px, py - 1
-        elif key == readchar.key.RIGHT:
-            new_px, new_py = px, py + 1
-        else:
-            continue
+        dimensions = lines[0].strip().split(',')
+        rows, cols = int(dimensions[0]), int(dimensions[1])
+        start = tuple(map(int, lines[1].strip().split(',')))
+        end = tuple(map(int, lines[2].strip().split(',')))
+        mapa = [list(line.strip()) for line in lines[3:]]
 
-        if 0 <= new_px < len(maze) and 0 <= new_py < len(maze[0]) and maze[new_px][new_py] != "#":
-            maze[px][py] = "."
-            px, py = new_px, new_py
-            maze[px][py] = "P"
+        return mapa, start, end
 
 def main():
-    print("¡Bienvenido al juego de laberinto!")
-    player_name = input("Por favor, ingresa tu nombre: ")
-    print(f"¡Hola, {player_name}! Comencemos a jugar.")
+    map_files_folder = "maps_folder"  # Ruta de la carpeta con archivos de mapas
+    map_files = os.listdir(map_files_folder)
 
-    maze_str = """
-    #######
-    #...D#
-    #.#.#.#
-    #.#.#.#
-    #...D#
-    #######
-    """
+    if not map_files:
+        print("No se encontraron archivos de mapas en la carpeta.")
+        return
 
-    start = (1, 1)
-    end = (4, 4)
+    map_file = random.choice(map_files)
+    map_file = os.path.join(map_files_folder, map_file)
 
-    maze = create_maze_matrix(maze_str)
-    main_loop(maze, start, end)
+    juego = JuegoArchivo(map_file)
+    juego.game_loop()
 
 if __name__ == "__main__":
     main()
